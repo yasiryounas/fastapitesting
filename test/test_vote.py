52229c6bf1278738a06aea0eafb100bb31a8e64d
yasiryounas <yasiryounas100@gmail.com>
2023-02-19 00:55:43 +0500

    Correction in vote test,

--- test/test_vote.py
+++ test/test_vote.py
@@ -5,7 +5,7 @@ from app import models
 def test_vote(test_posts,session,test_user):
     new_vote = models.Vote(post_id=test_posts[3].id,user_id=test_user['id'])
     session.add(new_vote)
-    session.commit
+    session.commit()
 
 def test_vote_on_post(authorized_client,test_posts):
     res = authorized_client.post("/vote/",json={"post_id": test_posts[0].id, "dir": 1})
@@ -13,7 +13,7 @@ def test_vote_on_post(authorized_client,test_posts):
 
 def test_vote_twice_post(authorized_client,test_posts,test_vote):
     res = authorized_client.post("/vote/", json={"post_id": test_posts[3].id, "dir": 1})
-    print(res.json())
+    #print(res.json())
     assert res.status_code == 409
 
 def test_delete_vote(authorized_client, test_posts, test_vote):
@@ -27,7 +27,7 @@ def test_delete_vote_non_exist(authorized_client, test_posts):
     assert res.status_code == 404
 
 
-def test_vote_post_non_exist(authorized_client, test_posts):
+def test_vote_post_non_exist(authorized_client, test_vote):
     res = authorized_client.post(
         "/vote/", json={"post_id": 80000, "dir": 1})
     assert res.status_code == 404
